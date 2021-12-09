44d1e01721bbd0092e5b655a4eb33323e306b98c
tanishqawasthy25 <54679324+tanishqawasthy25@users.noreply.github.com>
2021-12-09 11:20:05 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,14 +1,24 @@
 function average(numbers) {
+   if(!numbers.length)
+   {
+      return 0;
+   }
+   else
+   {
    let sum =0;
    let i=0;
    while(i<numbers.length)
    {
-      if(numbers[i]!==NaN)
+      
+      if(isNaN(numbers[i]))
       {
          sum=sum+numbers[i++];
       }
    }
+   
    return sum/numbers.length;
+   }
+  
 }
 
 module.exports={average}

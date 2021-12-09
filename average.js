b813f0d1293008e1ecf1cb408719ffb4580223a7
tanishqawasthy25 <54679324+tanishqawasthy25@users.noreply.github.com>
2021-12-09 10:48:45 +0530

    Update average.js

--- average.js
+++ average.js
@@ -1,6 +1,6 @@
-
-function average(numbers) {
-  return numbers.reduce((p, c)=> p + c, 0) / numbers.length;
+const average = (numbers) => {
+    const sumTotal =  numbers.reduce((acc, curr) => {
+        return Number(acc) + Number(curr);
+    }, 0);
+    return sumTotal/numbers?.length;
 }
-
-module.exports = {average};
